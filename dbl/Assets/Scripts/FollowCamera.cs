a19776a7c32d2333170ce9c50452306f2ea16208
StormProjects <tsb20@pitt.edu>
2018-12-28 14:43:52 -0500

    Update camera to follow player via script

---
+++ dbl/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+	public class FollowCamera : MonoBehaviour {
+	
+	public float xOffset = 0f; // Distance between player and camera in horizontal direction
+	
+	public float yOffset = 0f; // Distance between player and camera in vertical direction
+	
+	public Transform player; // Reference to the player's transform.
+
+	void LateUpdate() {
+		this.transform.position = new Vector3(player.transform.position.x +
+		xOffset, player.transform.position.y + yOffset, -10);
+	}
+}
