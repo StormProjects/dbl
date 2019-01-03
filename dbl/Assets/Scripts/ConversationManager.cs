d620c8de4179abbfcc26c701f3b95baf3eddb22e
StormProjects <tsb20@pitt.edu>
2019-01-02 23:41:59 -0500

    Add singleton class from unity docs, Create basic conversation manager

---
+++ dbl/Assets/Scripts/ConversationManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class ConversationManager : Singleton<ConversationManager> {
+ 
+	// To guarantee this will always be a singleton only, don't use the constructor
+	protected ConversationManager () {}
+
+	public void StartConversation(Conversation conversation) {
+		ConversationManager.Instance.StartConversation(conversation);
+	}
+
+}
