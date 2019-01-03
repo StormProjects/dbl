668c40fb6eb4b06a29336ea689e6580d9b8e63f1
StormProjects <tsb20@pitt.edu>
2019-01-02 21:39:22 -0500

    Add basic object serialization script

---
+++ dbl/Assets/Scripts/Editor/PositionManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+public class PositionManager : MonoBehaviour {
+	
+	[MenuItem("Assets/Create/PositionManager")] // Define a menu option in the editor to create the new asset
+	
+	public static void CreateAsset() {
+	
+		ScriptingObjects positionManager = ScriptableObject.CreateInstance<ScriptingObjects>(); // Create a new instance of our scriptable object
+		
+		AssetDatabase.CreateAsset(positionManager, "Assets/newPositionManager.asset"); // Create a .asset file for our new object and save it
+		AssetDatabase.SaveAssets();
+		
+		EditorUtility.FocusProjectWindow(); // Switch the inspector to our new object
+		Selection.activeObject = positionManager;
+	}
+
+	public static PositionManager ReadPositionsFromAsset(string Name) {
+		string path = "/";
+		object o = Resources.Load(path + Name);
+		PositionManager retrievedPositions = (PositionManager)o;
+		return retrievedPositions;
+	}
+}
---
+++ dbl/Assets/Scripts/ScriptingObjects.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public class ScriptingObjects : ScriptableObject {
+ 	public Vector2[] MyPositions;
+}
