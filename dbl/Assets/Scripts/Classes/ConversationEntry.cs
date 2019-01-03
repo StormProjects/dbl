d407651b38ffb728cbd1fc68d7b53dd6407089ef
StormProjects <tsb20@pitt.edu>
2019-01-02 23:34:59 -0500

    Create basic conversation system components, add portraits

---
+++ dbl/Assets/Scripts/Classes/ConversationEntry.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[System.Serializable] // tagged attribute required for custom classes intended to be used in serialization, otherwise Unity will not know
+
+public class ConversationEntry {
+
+	public string SpeakingCharacterName;
+	public string ConversationText;
+	public Sprite DisplayPortrait;
+	
+}
---
+++ dbl/Assets/Scripts/Editor/ConversationAssetCreator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+public class ConversationAssetCreator : MonoBehaviour {
+ 
+	[MenuItem("Assets/Create/Conversation")]
+
+	public static void CreateAsset() {
+	
+		CustomAssetUtility.CreateAsset<Conversation>();
+		
+	}
+
+}
