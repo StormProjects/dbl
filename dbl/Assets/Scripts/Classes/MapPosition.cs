5687b1d0dddf23a6a90408b45ff870c35e5c1bc0
StormProjects <tsb20@pitt.edu>
2019-02-03 02:18:00 -0500

    Create game state, map position scripts Add to prefab

---
+++ dbl/Assets/Scripts/Classes/MapPosition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Awake() {
+		var lastPosition = GameState.GetLastScenePosition(SceneManager.GetActiveScene().name);
+		if (lastPosition != Vector3.zero) {
+			transform.position = lastPosition;
+		}
+	}
+
+	void OnDestroy() {
+		GameState.SetLastScenePosition(SceneManager.GetActiveScene().name, transform.position);
+	}
+
+}
